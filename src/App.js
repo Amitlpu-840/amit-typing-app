624b1799bc6676390ad1f62aa0079312f8993ef8
Amit Giri <thisisag840@gmail.com>
2023-06-08 23:36:11 +0530

    geting random words

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './App.css'
+import {faker} from '@faker-js/faker'
+
+const words = faker.random.words(10);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Typing app for CHAABI
+      <header className="App-header text-3xl font-bold text-primary-500">
+        {words}
       </header>
     </div>
   );
